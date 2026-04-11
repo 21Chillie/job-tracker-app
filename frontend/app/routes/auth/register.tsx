8bee418a106760001747c062bd0c273c1e536acc
Chillie <reskialamsyah@gmail.com>
2026-04-12 05:20:22 +0800

    refactor: extract auth forms to components

--- frontend/app/routes/auth/register.tsx
+++ frontend/app/routes/auth/register.tsx
@@ -1,121 +1,26 @@
-import {
-  LuUserPlus,
-  LuMail,
-  LuKeyRound,
-  LuEye,
-  LuEyeOff,
-} from "react-icons/lu";
-import { FcGoogle } from "react-icons/fc";
-import { Link } from "react-router";
-import { useState } from "react";
+import RegisterForm from "@components/RegisterForm";
+import type { Route } from "./+types/register";
+import { authSchema } from "~/types/auth.type";
+import z from "zod";
+import { redirect } from "react-router";
 
 export default function Register() {
-  const [viewPassword, setViewpassword] = useState<boolean>(false);
-
   return (
     <>
-      <article className="bg-base-100 border-base-300 card mx-auto w-full max-w-md border shadow-lg max-sm:fixed max-sm:inset-0">
-        <header className="border-base-300 flex items-start gap-2 border-b p-6">
-          <div>
-            <LuUserPlus className="size-6" />
-          </div>
-
-          <div>
-            <h3 className="text-xl font-medium">Create new account</h3>
-            <p className="text-base-content/70 text-sm">
-              Quick and easy. Sign up to track your job applications.
-            </p>
-          </div>
-        </header>
-
-        <form className="pt-4 pb-6">
-          <div className="mb-4 space-y-1 px-6">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">What is your name?</legend>
-              <input
-                name="name"
-                id="name"
-                type="text"
-                className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full"
-                placeholder="John Doe"
-                required
-              />
-            </fieldset>
-
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Email</legend>
-              <label className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full">
-                <span>
-                  <LuMail className="text-base-content/50 size-4" />
-                </span>
-                <input
-                  name="email"
-                  id="email"
-                  type="email"
-                  placeholder="johndoe@mail.com"
-                  required
-                />
-              </label>
-            </fieldset>
-
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Password</legend>
-              <label className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full">
-                <span>
-                  <LuKeyRound className="text-base-content/50 size-4" />
-                </span>
-                <input
-                  name="password"
-                  id="password"
-                  type={viewPassword ? "text" : "password"}
-                  placeholder="Super secret password"
-                  required
-                />
-
-                <button
-                  type="button"
-                  onClick={() => setViewpassword(!viewPassword)}
-                >
-                  {viewPassword ? <LuEyeOff /> : <LuEye />}
-                </button>
-              </label>
-              <p className="label">
-                Must be 8+ characters, includes uppercase and number.
-              </p>
-            </fieldset>
-          </div>
-
-          <div className="space-y-4 px-6">
-            <button type="submit" className="btn btn-primary btn-block">
-              Register with email
-            </button>
-
-            <div className="flex items-center gap-3">
-              <div className="bg-base-content/30 h-px w-full"></div>
-              <p className="text-base-content/50 shrink-0 text-[11px]">
-                Or continue with Google
-              </p>
-              <div className="bg-base-content/30 h-px w-full"></div>
-            </div>
-
-            <button className="btn btn-block">
-              <span>
-                <FcGoogle />
-              </span>
-              Register with Google account
-            </button>
-          </div>
-        </form>
-
-        <div className="p-6 text-center">
-          <p className="text-base-content text-sm">
-            Already have an account?{" "}
-            <Link className="text-primary font-medium underline" to={"/login"}>
-              Log in
-            </Link>
-          </p>
-        </div>
-      </article>
+      <RegisterForm></RegisterForm>
     </>
   );
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const payload = Object.fromEntries(formData);
+
+  const validForm = authSchema.safeParse(payload);
+
+  if (!validForm.success) {
+    return { errors: z.treeifyError(validForm.error) };
+  }
+
+  return redirect("/");
 }
