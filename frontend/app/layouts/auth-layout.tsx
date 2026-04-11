f5ac4c44476828d5c6d795ae1eb17726f3fee8bc
Chillie <reskialamsyah@gmail.com>
2026-04-12 05:19:09 +0800

    update: adjust auth layout border radius

--- frontend/app/layouts/auth-layout.tsx
+++ frontend/app/layouts/auth-layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout() {
   return (
     <>
       <div className="bg-base-100 flex min-h-screen w-screen flex-col">
-        <main className="bg-base-200 m-3 grid flex-1 place-items-center rounded-lg">
+        <main className="bg-base-200 m-3 grid flex-1 place-items-center rounded-xl">
           <Outlet></Outlet>
         </main>
       </div>
