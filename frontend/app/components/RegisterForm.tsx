9b233149f231c7f68a3354c885feef84411ce412
Chillie <reskialamsyah@gmail.com>
2026-04-12 07:50:00 +0800

    update: refactor RegisterForm to use useRegisterForm hook

--- frontend/app/components/RegisterForm.tsx
+++ frontend/app/components/RegisterForm.tsx
@@ -8,9 +8,17 @@ import {
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router";
 import { useState } from "react";
+import { useRegisterForm } from "~/hooks/auth/register.hook";
 
 function RegisterForm() {
   const [viewPassword, setViewpassword] = useState<boolean>(false);
+  const { handleSubmit, Field, Subscribe } = useRegisterForm();
+
+  const handleRegisterForm = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    handleSubmit();
+  };
 
   return (
     <>
@@ -28,68 +36,143 @@ function RegisterForm() {
           </div>
         </header>
 
-        <form className="pt-4 pb-6">
+        <form className="pt-4 pb-6" onSubmit={handleRegisterForm}>
           <div className="mb-4 space-y-1 px-6">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">What is your name?</legend>
-              <input
-                name="name"
-                id="name"
-                type="text"
-                className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full"
-                placeholder="John Doe"
-                required
-              />
-            </fieldset>
+            <Field name="name">
+              {(field) => {
+                const { errors, isTouched } = field.state.meta;
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Email</legend>
-              <label className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full">
-                <span>
-                  <LuMail className="text-base-content/50 size-4" />
-                </span>
-                <input
-                  name="email"
-                  id="email"
-                  type="email"
-                  placeholder="johndoe@mail.com"
-                  required
-                />
-              </label>
-            </fieldset>
+                return (
+                  <fieldset className="fieldset">
+                    <legend className="fieldset-legend">
+                      What is your name?
+                    </legend>
+                    <input
+                      name={field.name}
+                      id={field.name}
+                      type="text"
+                      className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full"
+                      placeholder="John Doe"
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Password</legend>
-              <label className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full">
-                <span>
-                  <LuKeyRound className="text-base-content/50 size-4" />
-                </span>
-                <input
-                  name="password"
-                  id="password"
-                  type={viewPassword ? "text" : "password"}
-                  placeholder="Super secret password"
-                  required
-                />
+                    {errors.length > 0 && isTouched && (
+                      <div className="space-y-1">
+                        {errors.map((error, index) => (
+                          <p className="text-error label" key={index}>
+                            {error?.message}
+                          </p>
+                        ))}
+                      </div>
+                    )}
+                  </fieldset>
+                );
+              }}
+            </Field>
 
-                <button
-                  type="button"
-                  onClick={() => setViewpassword(!viewPassword)}
-                >
-                  {viewPassword ? <LuEyeOff /> : <LuEye />}
-                </button>
-              </label>
+            <Field name="email">
+              {(field) => {
+                const { errors, isTouched } = field.state.meta;
 
-              <p className="label">
-                Must be 8+ characters, includes uppercase and number.
-              </p>
-            </fieldset>
+                return (
+                  <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Email</legend>
+                    <label className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full">
+                      <span>
+                        <LuMail className="text-base-content/50 size-4" />
+                      </span>
+                      <input
+                        name={field.name}
+                        id={field.name}
+                        type="email"
+                        placeholder="johndoe@mail.com"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                    </label>
+
+                    {errors.length > 0 && isTouched && (
+                      <div className="space-y-1">
+                        {errors.map((error, index) => (
+                          <p className="text-error label" key={index}>
+                            {error?.message}
+                          </p>
+                        ))}
+                      </div>
+                    )}
+                  </fieldset>
+                );
+              }}
+            </Field>
+
+            <Field name="password">
+              {(field) => {
+                const { errors, isTouched } = field.state.meta;
+
+                return (
+                  <fieldset className="fieldset">
+                    <legend className="fieldset-legend">Password</legend>
+                    <label className="input focus-within:outline-primary/20 focus-within:border-primary/50 w-full">
+                      <span>
+                        <LuKeyRound className="text-base-content/50 size-4" />
+                      </span>
+                      <input
+                        name={field.name}
+                        id={field.name}
+                        type={viewPassword ? "text" : "password"}
+                        placeholder="Super secret password"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+
+                      <button
+                        type="button"
+                        onClick={() => setViewpassword(!viewPassword)}
+                      >
+                        {viewPassword ? <LuEyeOff /> : <LuEye />}
+                      </button>
+                    </label>
+
+                    {errors.length > 0 && isTouched ? (
+                      <div>
+                        {errors.map((error, index) => (
+                          <p key={index} className="text-error label">
+                            {error?.message}
+                          </p>
+                        ))}
+                      </div>
+                    ) : (
+                      <p className="label">
+                        Must be 8+ characters, includes uppercase and number.
+                      </p>
+                    )}
+                  </fieldset>
+                );
+              }}
+            </Field>
           </div>
 
           <div className="space-y-4 px-6">
-            <button type="submit" className="btn btn-primary btn-block">
-              Register with email
-            </button>
+            <Subscribe
+              selector={(state) => [state.isSubmitting, state.canSubmit]}
+            >
+              {([isSubmitting, canSubmit]) => (
+                <button
+                  type="submit"
+                  disabled={isSubmitting || !canSubmit}
+                  className="btn btn-primary btn-block"
+                >
+                  {isSubmitting ? (
+                    <span className="loading loading-spinner"></span>
+                  ) : null}{" "}
+                  Register with email
+                </button>
+              )}
+            </Subscribe>
 
             <div className="flex items-center gap-3">
               <div className="bg-base-content/30 h-px w-full"></div>
