37802bc04624d842a51b3b8f41d730efd4834e4c
Chillie <reskialamsyah@gmail.com>
2026-04-12 23:53:56 +0800

    feat: add script to initialize auth schema
    This script checks if the 'user' table exists in the database. If it
    doesn't, it executes the SQL schema file to initialize the
    authentication-related tables.

---
+++ backend/src/scripts/auth-migrate.ts
@@ -0,0 +1,29 @@
+import db from "@db/db-client";
+import { readFileSync } from "fs";
+import { join } from "path";
+import logger from "~/config/logger";
+
+export function authMigrations() {
+  const schemaPath = join(__dirname, "../db/schemas/auth.schema.sql");
+
+  try {
+    const sql = readFileSync(schemaPath, "utf8");
+
+    // check if user table is exist
+    const tableExists = db
+      .query(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='user'",
+      )
+      .get();
+
+    if (!tableExists) {
+      // execute the entire generated SQL file
+      db.run(sql);
+      logger.info("Database schema initialized.");
+    }
+  } catch (error) {
+    logger.fatal("Migration failed");
+    console.error(error);
+    process.exit(1);
+  }
+}
