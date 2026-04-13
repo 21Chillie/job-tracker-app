7e393afb64f8cb4ebad6d52a4b4107b7226e112b
Chillie <reskialamsyah@gmail.com>
2026-04-14 02:06:41 +0800

    update: add dashboard routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -11,5 +11,10 @@ export default [
     route("register", "./routes/auth/register.tsx"),
   ]),
 
-  route("/", "./layouts/dashboard-layout.tsx", [index("./routes/home.tsx")]),
+  route("/", "./layouts/dashboard-layout.tsx", [
+    index("./routes/dashboard/home.tsx"),
+    route("jobs", "./routes/dashboard/job-list.tsx"),
+    route("add-job", "./routes/dashboard/add-job.tsx"),
+    route("profile", "./routes/dashboard/profile.tsx"),
+  ]),
 ] satisfies RouteConfig;
