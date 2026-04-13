d5ba135d964243ae3991816333e444cb1ead2295
Chillie <reskialamsyah@gmail.com>
2026-04-14 02:08:35 +0800

    update: add loading indicator to AuthLayout

--- frontend/app/layouts/auth-layout.tsx
+++ frontend/app/layouts/auth-layout.tsx
@@ -1,11 +1,19 @@
+import { useNavigation } from "react-router";
 import { Outlet } from "react-router";
 
 export default function AuthLayout() {
+  const navigation = useNavigation();
+  const pageIsLoading = navigation.state === "loading";
+
   return (
     <>
       <div className="bg-base-100 flex min-h-screen w-screen flex-col">
         <main className="bg-base-200 m-3 grid flex-1 place-items-center rounded-xl">
-          <Outlet></Outlet>
+          {pageIsLoading ? (
+            <span className="loading loading-spinner text-primary"></span>
+          ) : (
+            <Outlet></Outlet>
+          )}
         </main>
       </div>
     </>
