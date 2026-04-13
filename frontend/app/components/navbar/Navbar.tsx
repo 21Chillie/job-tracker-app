60bc98b48f6ca654906a40e73d45ca89f740c385
Chillie <reskialamsyah@gmail.com>
2026-04-14 02:10:56 +0800

    add: Navbar and ProfileDropdownMenu components

---
+++ frontend/app/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import type { UserNavbarType } from "~/types/global.type";
+import ProfileDropdownMenu from "./ProfileDropdownMenu";
+
+export default function Navbar({ user }: { user: UserNavbarType }) {
+  return (
+    <>
+      <header className="bg-base-100 flex shrink-0 items-center justify-end p-3 md:p-4">
+        <div className="flex items-center gap-4">
+          <button className="btn btn-ghost btn-sm">Dashboard</button>
+          <a
+            href="https://github.com/21Chillie/job-tracker-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-ghost btn-sm"
+          >
+            Documentations
+          </a>
+
+          <ProfileDropdownMenu {...user}></ProfileDropdownMenu>
+        </div>
+      </header>
+    </>
+  );
+}
