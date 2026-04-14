b64dae44c1922cb09688848f71179a9792e8a834
Chillie <reskialamsyah@gmail.com>
2026-04-14 09:47:14 +0800

    add: job form component and client action (not finish)

--- frontend/app/routes/dashboard/add-job.tsx
+++ frontend/app/routes/dashboard/add-job.tsx
@@ -1,3 +1,22 @@
+import AddJobForm from "@components/AddJobForm";
+import type { Route } from "./+types/add-job";
+import toast from "react-hot-toast";
+
 export default function AddJob() {
-  return <></>;
+  return (
+    <>
+      <AddJobForm />
+    </>
+  );
+}
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+  const payload = Object.fromEntries(formData);
+
+  if (payload) {
+    toast.success("Job added successfully");
+  }
+
+  return null;
 }
