ae6cf201a1ac7d74f33fc348dedfcbdc0afae7a3
Chillie <reskialamsyah@gmail.com>
2026-04-15 01:46:56 +0800

    update: add client action to handle form data

--- frontend/app/routes/dashboard/add-job.tsx
+++ frontend/app/routes/dashboard/add-job.tsx
@@ -1,8 +1,9 @@
 import AddJobForm from "@components/AddJobForm";
 import type { Route } from "./+types/add-job";
 import toast from "react-hot-toast";
+import type { JobApplicationFormType } from "~/types/job.type";
 
-export default function AddJob() {
+export default function AddJob({ actionData }: Route.ComponentProps) {
   return (
     <>
       <AddJobForm />
@@ -12,11 +13,16 @@ export default function AddJob() {
 
 export async function clientAction({ request }: Route.ClientActionArgs) {
   const formData = await request.formData();
-  const payload = Object.fromEntries(formData);
+  const payload = Object.fromEntries(
+    formData,
+  ) as unknown as JobApplicationFormType;
 
-  if (payload) {
-    toast.success("Job added successfully");
+  if (!payload) {
+    toast.error("Please fill in all fields");
+    return { data: null };
   }
 
-  return null;
+  toast.success("Job added successfully");
+
+  return { data: payload };
 }
