30330d22466bbdbbcb92cda740ba40002c5b7555
Chillie <reskialamsyah@gmail.com>
2026-04-15 08:11:45 +0800

    update: add-job route to use axios for API calls (refactor it later using useMutation from Tanstack Query)

--- frontend/app/routes/dashboard/add-job.tsx
+++ frontend/app/routes/dashboard/add-job.tsx
@@ -1,9 +1,10 @@
 import AddJobForm from "@components/AddJobForm";
 import type { Route } from "./+types/add-job";
 import toast from "react-hot-toast";
-import type { JobApplicationFormType } from "~/types/job.type";
+import type { JobsApplicationFormType, JobsApiResponse } from "~/types/job.type";
+import api from "~/configs/axios-instance.config";
 
-export default function AddJob({ actionData }: Route.ComponentProps) {
+export default function AddJob() {
   return (
     <>
       <AddJobForm />
@@ -15,14 +16,19 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
   const formData = await request.formData();
   const payload = Object.fromEntries(
     formData,
-  ) as unknown as JobApplicationFormType;
+  ) as unknown as JobsApplicationFormType;
 
   if (!payload) {
     toast.error("Please fill in all fields");
     return { data: null };
   }
 
-  toast.success("Job added successfully");
+  // Change this later using tanstack query
+  const result = await api.post<JobsApiResponse>("/api/jobs", payload);
+
+  if (result.data.success) {
+    toast.success("Job added successfully");
+  }
 
   return { data: payload };
 }
