b05fa63ab249a062d93a022641c15df3f55e5284
Chillie <reskialamsyah@gmail.com>
2026-04-15 08:18:52 +0800

    feat: Add check-auth middleware
    This middleware verifies the user's session by checking for session data
    in the request headers. If a valid session is found, it attaches the
    user and session data to the context. Otherwise, it throws an
    unauthorized exception.

---
+++ backend/src/middlewares/check-auth.middleware.ts
@@ -0,0 +1,25 @@
+import { createMiddleware } from "hono/factory";
+import { auth } from "@utils/auth";
+import { HTTPException } from "hono/http-exception";
+import logger from "~/config/logger";
+
+export const checkAuth = createMiddleware(async (c, next) => {
+  // Check user session data
+  const session = await auth.api.getSession({
+    headers: c.req.raw.headers,
+  });
+
+  if (!session) {
+    logger.debug("User session is not found");
+    throw new HTTPException(401, {
+      message: "User session is not found",
+    });
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+
+  logger.debug("User session is found");
+
+  await next();
+});
