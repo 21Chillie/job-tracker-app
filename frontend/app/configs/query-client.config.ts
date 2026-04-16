306bad5212d3fd1ea2b91b1cbac8b2c764b94c2c
Chillie <reskialamsyah@gmail.com>
2026-04-17 04:05:39 +0800

    update: refactor query client configuration
    Introduce a factory function `getQueryClient` to handle client
    instantiation. This ensures a single instance of the QueryClient is used
    on the browser, while a new instance is created on the server for each
    request.

--- frontend/app/configs/query-client.config.ts
+++ frontend/app/configs/query-client.config.ts
@@ -12,11 +12,23 @@ const queryClientConfig: QueryClientConfig = {
 let browserQueryClient: QueryClient | undefined = undefined;
 
 export function getQueryClient() {
+  // 'window' only exists in browsers. If it's undefined, we are on the Server
   if (typeof window === "undefined") {
+    /**
+     * Always return a BRAND NEW client for the server.
+     * Since this is a new object, it has an empty cache.
+     * This prevents User A's data from accidentally leaking to User B. (Security)
+     */
+
     return new QueryClient(queryClientConfig);
   } else {
     if (!browserQueryClient)
       browserQueryClient = new QueryClient(queryClientConfig);
+
+  /**
+   * Return the SAME QueryClient every single time.
+   * Because it's the same object, it keeps all cached data (like the user session) even when navigate between pages.
+   */
     return browserQueryClient;
   }
 }
