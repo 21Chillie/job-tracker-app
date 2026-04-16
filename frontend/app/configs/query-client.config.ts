11e4113c88f51af599f2380bb6090ab3acf4b3dd
Chillie <reskialamsyah@gmail.com>
2026-04-17 04:04:59 +0800

    update: refactor query client configuration
    Introduce a factory function `getQueryClient` to handle client
    instantiation. This ensures a single instance of the QueryClient is used
    on the browser, while a new instance is created on the server for each
    request.

--- frontend/app/configs/query-client.config.ts
+++ frontend/app/configs/query-client.config.ts
@@ -1,11 +1,24 @@
-import { type QueryClientConfig } from "@tanstack/react-query";
+import { QueryClient, type QueryClientConfig } from "@tanstack/react-query";
 
 const queryClientConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
+      // refetchOnWindowFocus: false,
     },
   },
 };
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (typeof window === "undefined") {
+    return new QueryClient(queryClientConfig);
+  } else {
+    if (!browserQueryClient)
+      browserQueryClient = new QueryClient(queryClientConfig);
+    return browserQueryClient;
+  }
+}
 
 export default queryClientConfig;
