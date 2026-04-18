0e556bb2c65bbe958cb3a3ca88f86a79b81da508
Chillie <reskialamsyah@gmail.com>
2026-04-18 15:13:32 +0800

    add: profile Form component
    Implement the ProfileForm component for updating user profile details
    and managing password changes. Includes avatar selection and a logout
    modal.

---
+++ frontend/app/components/profile/avatar-profile.ts
@@ -0,0 +1,12 @@
+const avatarProfileImg = [
+  { id: 1, file: "./avatar-placeholder-1.webp" },
+  { id: 2, file: "./avatar-placeholder-2.webp" },
+  { id: 3, file: "./avatar-placeholder-3.webp" },
+  { id: 4, file: "./avatar-placeholder-4.webp" },
+  { id: 5, file: "./avatar-placeholder-5.webp" },
+  { id: 6, file: "./avatar-placeholder-6.webp" },
+  { id: 7, file: "./avatar-placeholder-7.webp" },
+  { id: 8, file: "./avatar-placeholder-8.webp" },
+];
+
+export default avatarProfileImg;
