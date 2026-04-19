dcea5060894b5f8c7730f9e22478777694a3490e
Chillie <reskialamsyah@gmail.com>
2026-04-20 02:54:13 +0800

    update: refactor job application form data structure and validation schema

--- backend/src/models/jobs/jobs.model.ts
+++ backend/src/models/jobs/jobs.model.ts
@@ -1,38 +1,36 @@
 import db from "@db/db-client";
 import logger from "@config/logger";
 import { JobApplicationFormDataType } from "~/types/jobs.types";
+import { randomUUIDv7 } from "bun";
 
 const jobsModel = {
-  create: async (userId: string, data: JobApplicationFormDataType) => {
-    const id = Bun.randomUUIDv7();
-    const { fJobTitle, fCompany, fJobUrl, fJobStatus, fAppliedDate, fNotes } =
-      data;
+	create: async (userId: string, data: JobApplicationFormDataType) => {
+		const id = randomUUIDv7();
+		const { position, company, jobUrl, status, appliedDate, notes } = data;
 
-    const query = db.query(
-      "INSERT INTO job (id, user_id, job_title, company, job_url, job_status, applied_date, notes) VALUES ($id, $user_id, $job_title, $company, $job_url, $job_status, $applied_date, $notes) RETURNING *;",
-    );
+		const query = db.query(
+			"INSERT INTO job (id, user_id, job_title, company, job_url, job_status, applied_date, notes) VALUES ($id, $user_id, $job_title, $company, $job_url, $job_status, $applied_date, $notes) RETURNING *;",
+		);
 
-    try {
-      const result = query.get({
-        id,
-        user_id: userId,
-        job_title: fJobTitle,
-        company: fCompany,
-        job_url: fJobUrl || null,
-        job_status: fJobStatus,
-        applied_date: fAppliedDate,
-        notes: fNotes || null,
-      });
+		try {
+			const result = query.get({
+				id,
+				user_id: userId,
+				job_title: position,
+				company: company,
+				job_url: jobUrl || null,
+				job_status: status,
+				applied_date: appliedDate || null,
+				notes: notes || null,
+			});
 
-      logger.info(
-        `Job created successfully. [${id}, ${userId.slice(0, 10)}...]`,
-      );
+			logger.info(`Job created successfully. [${id}, ${userId.slice(0, 10)}...]`);
 
-      return result;
-    } catch (err) {
-      throw new Error((err as Error).message);
-    }
-  },
+			return result;
+		} catch (err) {
+			throw new Error((err as Error).message);
+		}
+	},
 };
 
 export default jobsModel;
--- backend/src/types/jobs.types.ts
+++ backend/src/types/jobs.types.ts
@@ -1,21 +1,14 @@
 import z from "zod";
 
 export const JobFormSchema = z.object({
-  fJobTitle: z.string().min(1, "Job title is required").max(100),
-  fCompany: z.string().min(1, "Company name is required"),
-  fJobUrl: z.string().url("Invalid URL").optional().or(z.literal("")),
-  fJobStatus: z.enum([
-    "applied",
-    "tested",
-    "interviewed",
-    "offer",
-    "accepted",
-    "rejected",
-  ]),
-  fAppliedDate: z
-    .string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format (YYYY-MM-DD)"),
-  fNotes: z.string().max(500).optional(),
+	position: z.string().min(1, { error: "You must provide value for position field" }),
+	company: z.string().min(1, { error: "You must provide value for company field" }),
+	jobUrl: z.url({ error: "Invalid URL address" }).optional().or(z.literal("")),
+	status: z.enum(["applied", "tested", "interviewed", "offer", "accepted", "rejected"], {
+		error: "Invalid status value",
+	}),
+	appliedDate: z.iso.date({ error: "Invalid date format (YYYY-MM-DD)" }).optional().or(z.literal("")),
+	notes: z.string().max(500).optional().or(z.literal("")),
 });
 
 export type JobApplicationFormDataType = z.infer<typeof JobFormSchema>;
