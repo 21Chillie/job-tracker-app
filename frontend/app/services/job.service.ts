e04efcd47d069343346b428913fc3a7e1feb8f03
Chillie <reskialamsyah@gmail.com>
2026-04-20 15:46:42 +0800

    update: implement job service for creating new user jobs

---
+++ frontend/app/services/job.service.ts
@@ -0,0 +1,24 @@
+import api from "~/configs/axios-instance.config";
+import type { FormJobDataType } from "@hooks/job/useJobFom.hook";
+import type { JobsApiResponse } from "~/types/job.type";
+
+const jobService = {
+  newUserJob: async ({
+    userId,
+    formData,
+  }: {
+    userId: string;
+    formData: FormJobDataType;
+  }) => {
+    if (!userId) throw new Response("User ID is required");
+
+    const result = await api.post<JobsApiResponse>("/api/jobs/new", {
+      ...formData,
+      userId,
+    });
+
+    return result.data;
+  },
+};
+
+export default jobService;
