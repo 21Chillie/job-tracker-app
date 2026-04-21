4f616130d7bcec1a0a1dcf1602c3712a4fcbfb42
Chillie <reskialamsyah@gmail.com>
2026-04-21 08:22:39 +0800

    update: render form delete account and also with delete account functionality and refactor clientAction logic

--- frontend/app/routes/dashboard/profile.tsx
+++ frontend/app/routes/dashboard/profile.tsx
@@ -1,16 +1,19 @@
+import { DangerousZone } from "@components/profile/DangerZone";
 import ProfileForm from "@components/profile/ProfileForm";
-import type { profileFormDataType } from "@hooks/user/useEditProfile.hook";
-import type { Route } from "./+types/profile";
-import profileService from "~/services/profile.service";
-import toast from "react-hot-toast";
-import { redirect } from "react-router";
 import { getQueryClient } from "@configs/query-client.config";
 import { sessionQueryOption } from "@hooks/auth/useSession.hook";
+import type { profileFormDataType } from "@hooks/user/useEditProfile.hook";
+import authService from "@services/auth.service";
+import profileService from "@services/profile.service";
+import toast from "react-hot-toast";
+import { redirect } from "react-router";
+import type { Route } from "./+types/profile";
 
 export default function Profile() {
   return (
     <>
       <ProfileForm></ProfileForm>
+      <DangerousZone></DangerousZone>
     </>
   );
 }
@@ -18,28 +21,55 @@ export default function Profile() {
 export async function clientAction({ request }: Route.ClientActionArgs) {
   const queryClient = getQueryClient();
   const formData = await request.formData();
-  const payload = Object.fromEntries(
-    formData,
-  ) as unknown as profileFormDataType;
 
-  const { name, avatar, currentPassword, newPassword } = payload;
+  // This will decide which action will do:O
+  // It's either `edit-profile` and `delete-account`
+  const actionIntent = formData.get("action");
 
   try {
-    if (name) {
-      await profileService.update({ name, image: avatar });
+    switch (actionIntent) {
+      case "edit-profile": {
+        const data = Object.fromEntries(
+          formData,
+        ) as unknown as profileFormDataType;
+
+        const { name, avatar, currentPassword, newPassword } = data;
+
+        if (name) {
+          await profileService.update({ name, image: avatar });
+        }
+
+        if (currentPassword && newPassword) {
+          await profileService.changePassword({
+            currentPassword,
+            newPassword,
+          });
+        }
+
+        await queryClient.invalidateQueries(sessionQueryOption());
+
+        toast.success("Profile updated successfully");
+        return redirect("/profile");
+      }
+
+      case "delete-account": {
+        const data = Object.fromEntries(formData) as unknown as {
+          password: string;
+        };
+
+        if (!data.password || data.password.trim().length === 0) {
+          return toast.error("Password is required to delete account");
+        }
+
+        const deleteSuccess = await authService.deleteAccount(data.password);
+
+        if (deleteSuccess.success) {
+          toast.success("Account deleted successfully");
+          await queryClient.invalidateQueries(sessionQueryOption());
+          return redirect("/login");
+        }
+      }
     }
-
-    if (currentPassword && newPassword) {
-      await profileService.changePassword({
-        currentPassword,
-        newPassword,
-      });
-    }
-
-    await queryClient.invalidateQueries(sessionQueryOption());
-
-    toast.success("Profile updated successfully");
-    return redirect("/profile");
   } catch (err) {
     const errorMessage =
       err instanceof Error
