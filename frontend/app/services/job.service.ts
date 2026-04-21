8b1ffe0c779904c100390b0ddbbc9d84851ead07
Chillie <reskialamsyah@gmail.com>
2026-04-22 07:35:20 +0800

    update: add service to get user jobs data (with pagination and query params)

--- frontend/app/services/job.service.ts
+++ frontend/app/services/job.service.ts
@@ -1,6 +1,10 @@
-import api from "~/configs/axios-instance.config";
+import api from "@configs/axios-instance.config";
 import type { FormJobDataType } from "@hooks/job/useJobFom.hook";
-import type { JobsApiResponse } from "~/types/job.type";
+import type {
+  JobDataResponse,
+  JobsDataResponse,
+  QueryJobsParams,
+} from "~/types/job.type";
 
 const jobService = {
   newUserJob: async ({
@@ -12,14 +16,31 @@ const jobService = {
   }) => {
     if (!userId) throw new Response("User ID is required");
 
-    // TODO: need to handle formData validation
-
-    const result = await api.post<JobsApiResponse>("/api/jobs/new", {
+    const result = await api.post<JobDataResponse>("/api/jobs/new", {
       ...formData,
       userId,
     });
 
     return result.data;
+  },
+
+  getUserJobs: async (cookie?: string, params?: QueryJobsParams) => {
+    const response = await api.get<JobsDataResponse>("/api/jobs", {
+      params: {
+        search: params?.search,
+        status: params?.status,
+        sortBy: params?.sortBy,
+        order: params?.order,
+        page: params?.page,
+        limit: params?.limit,
+      },
+      headers: {
+        // Forward the user's cookie to the backend
+        Cookie: cookie,
+      },
+    });
+
+    return response.data;
   },
 };
 
