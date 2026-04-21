9bdf851f5d5a774180bec2beaaded007c8df1b79
Chillie <reskialamsyah@gmail.com>
2026-04-22 07:30:29 +0800

    update: implement lazy loading for AddJobForm

--- frontend/app/routes/dashboard/add-job.tsx
+++ frontend/app/routes/dashboard/add-job.tsx
@@ -1,15 +1,23 @@
-import AddJobForm from "@components/job-page/add-job/AddJobForm";
-import toast from "react-hot-toast";
-import type { Route } from "./+types/add-job";
+import { AddJobFormSkeleton } from "@components/job-page/add-job/AddJobFormSkeleton";
+import { getQueryClient } from "@configs/query-client.config";
+import { sessionQueryOption } from "@hooks/auth/useSession.hook";
+import { jobsDataOption } from "@hooks/job/useJobData.hook";
 import type { FormJobDataType } from "@hooks/job/useJobFom.hook";
 import jobService from "@services/job.service";
-import { sessionQueryOption } from "@hooks/auth/useSession.hook";
-import { getQueryClient } from "@configs/query-client.config";
+import { Suspense, lazy } from "react";
+import toast from "react-hot-toast";
+import type { Route } from "./+types/add-job";
+
+const AddJobForm = lazy(
+  () => import("@components/job-page/add-job/AddJobForm"),
+);
 
 export default function AddJob() {
   return (
     <>
-      <AddJobForm />
+      <Suspense fallback={<AddJobFormSkeleton />}>
+        <AddJobForm />
+      </Suspense>
     </>
   );
 }
@@ -34,8 +42,7 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
   if (result.success) {
     toast.success("Job added successfully");
 
-    // TODO: you need to invalidate job cache after adding new
-    // queryClient.invalidateQueries();
+    queryClient.invalidateQueries(jobsDataOption());
   }
 
   return { data: payload };
