5ae2e1ef66b360476ff4b6fd0b06a034a08f51a8
Chillie <reskialamsyah@gmail.com>
2026-04-22 11:35:43 +0800

    update: add pending state that show loading when fetch data

--- frontend/app/components/job-page/job-list/JobTable.tsx
+++ frontend/app/components/job-page/job-list/JobTable.tsx
@@ -1,12 +1,11 @@
-import { dataJobsPlaceholder } from "./dataPlaceholder";
-import { TableRow } from "./TableRow";
-import { ChevronRight, ChevronLeft } from "lucide-react";
-import Filter from "./Filter";
-import { useAppDispatch, useAppSelector } from "~/configs/store.config";
 import { useQuery } from "@tanstack/react-query";
-import { jobsDataOption } from "~/hooks/job/useJobData.hook";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useDebounce } from "use-debounce";
+import { useAppDispatch, useAppSelector } from "~/configs/store.config";
 import { setNextPage, setPrevPage } from "~/features/job/jobFilterSlice";
+import { jobsDataOption } from "~/hooks/job/useJobData.hook";
+import Filter from "./Filter";
+import { TableRow } from "./TableRow";
 
 export default function JobTable() {
   const filters = useAppSelector((state) => state.jobFilter);
@@ -18,14 +17,12 @@ export default function JobTable() {
     search: debouncedSearch,
   };
 
-  const { data, isPlaceholderData } = useQuery(
+  const { data, isPlaceholderData, isPending } = useQuery(
     jobsDataOption(undefined, queryParams),
   );
 
-  if (!data) return null;
-
-  const { jobs, meta } = data.data;
-  const { total, page, limit, maxPages, hasNextPage, hasPrevPage } = meta;
+  const jobs = data?.data.jobs || [];
+  const meta = data?.data?.meta;
 
   return (
     <>
@@ -35,86 +32,91 @@ export default function JobTable() {
       >
         <Filter />
 
-        <article
-          className={`${isPlaceholderData ? "opacity-50" : "opacity-100"} border-base-300 bg-base-100 rounded-box border shadow-xl`}
-        >
-          <div className="overflow-x-auto pb-4">
-            <table className="table-sm md:table-md table min-w-4xl max-sm:min-w-3xl">
-              {/* table head */}
-              <thead>
-                <tr>
-                  {/*<th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>*/}
-                  <th>Position</th>
-                  <th>Company</th>
-                  <th>Status</th>
-                  <th>Date Applied</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-
-              {/* table body */}
-              <tbody>
-                {jobs.map((job, i) => {
-                  return <TableRow key={i} {...job} />;
-                })}
-              </tbody>
-            </table>
+        {isPending && !isPlaceholderData ? (
+          <div className="relative">
+            <div className="absolute inset-y-100 flex w-full items-center justify-center">
+              <p className="skeleton skeleton-text w-fit font-medium">
+                Getting job data from server, please be patient...
+              </p>
+            </div>
           </div>
+        ) : (
+          <article
+            className={`${isPlaceholderData ? "opacity-50" : "opacity-100"} border-base-300 bg-base-100 rounded-box border shadow-xl`}
+          >
+            <div className="overflow-x-auto pb-4">
+              <table className="table-sm md:table-md table min-w-4xl max-sm:min-w-3xl">
+                {/* table head */}
+                <thead>
+                  <tr>
+                    <th>Position</th>
+                    <th>Company</th>
+                    <th>Status</th>
+                    <th>Date Applied</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
 
-          {/* TODO: change this later */}
-          {jobs.length > 0 ? (
-            <div className="flex items-center justify-between px-4 py-3 md:px-6">
-              <p className="label text-sm whitespace-normal">
-                Showing {total} jobs
-              </p>
+                {/* table body */}
+                <tbody>
+                  {jobs.map((job, i) => {
+                    return <TableRow key={i} {...job} />;
+                  })}
+                </tbody>
+              </table>
+            </div>
 
-              <div className="join border-base-300 rounded-full border shadow-md">
-                <button
-                  type="button"
-                  className="join-item btn btn-sm btn-soft"
-                  disabled={!hasPrevPage}
-                  onClick={() => {
-                    dispatch(setPrevPage());
-                  }}
-                >
-                  <span>
-                    <ChevronLeft
-                      className={`${hasPrevPage ? "text-base-content" : "text-base-content/40"} size-4`}
-                    />
-                  </span>
-                </button>
+            {/* table footer */}
+            {meta && jobs.length > 0 ? (
+              <div className="flex items-center justify-between px-4 py-3 md:px-6">
+                <p className="label text-sm whitespace-normal">
+                  Showing {meta.total} jobs
+                </p>
 
-                <button
-                  type="button"
-                  className="join-item btn btn-sm btn-soft text-base-content"
-                >
-                  <p>{`${page} / ${maxPages}`}</p>
-                </button>
+                <div className="join border-base-300 rounded-full border shadow-md">
+                  <button
+                    type="button"
+                    className="join-item btn btn-sm btn-soft"
+                    disabled={!meta.hasPrevPage}
+                    onClick={() => {
+                      dispatch(setPrevPage());
+                    }}
+                  >
+                    <span>
+                      <ChevronLeft
+                        className={`${meta.hasPrevPage ? "text-base-content" : "text-base-content/40"} size-4`}
+                      />
+                    </span>
+                  </button>
 
-                <button
-                  type="button"
-                  disabled={!hasNextPage}
-                  className="join-item btn btn-sm btn-soft"
-                  onClick={() => dispatch(setNextPage())}
-                >
-                  <span>
-                    <ChevronRight className="text-base-content size-4" />
-                  </span>
-                </button>
+                  <button
+                    type="button"
+                    className="join-item btn btn-sm btn-soft text-base-content"
+                  >
+                    <p>{`${meta.page} / ${meta.maxPages}`}</p>
+                  </button>
+
+                  <button
+                    type="button"
+                    disabled={!meta.hasNextPage}
+                    className="join-item btn btn-sm btn-soft"
+                    onClick={() => dispatch(setNextPage())}
+                  >
+                    <span>
+                      <ChevronRight className="text-base-content size-4" />
+                    </span>
+                  </button>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="px-4 py-3 text-center md:px-6">
-              <p className="label text-sm whitespace-normal">
-                No data has been added yet
-              </p>
-            </div>
-          )}
-        </article>
+            ) : (
+              <div className="px-4 py-3 text-center md:px-6">
+                <p className="label text-sm whitespace-normal">
+                  No data has been added yet
+                </p>
+              </div>
+            )}
+          </article>
+        )}
       </section>
     </>
   );
