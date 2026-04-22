514f9030e2a62e91d82ace24ea1783a99f39dcdc
Chillie <reskialamsyah@gmail.com>
2026-04-22 11:37:49 +0800

    update: add jobFilterReducer in redux toolkit store

--- frontend/app/configs/store.config.ts
+++ frontend/app/configs/store.config.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import authReducer from "@features/auth/authSlice";
+import jobFilterReducer from "@features/job/jobFilterSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    jobFilter: jobFilterReducer,
   },
 });
 
