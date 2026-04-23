633ab979d19f3187a13271871648f9c3627c4ad1
Chillie <reskialamsyah@gmail.com>
2026-04-23 15:19:59 +0800

    update: add jobs route to handle update data and also validate form data from client side

--- backend/src/routes/jobs.route.ts
+++ backend/src/routes/jobs.route.ts
@@ -4,6 +4,7 @@ import { checkAuth } from "@middlewares/check-auth.middleware";
 import { generalLimiter } from "@middlewares/rateLimiter.middleware";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import z from "zod";
 import { Variables } from "~/types/global.types";
 import { deleteFormSchema, JobFormSchema } from "~/types/jobs.types";
 
@@ -28,9 +29,24 @@ jobsRoute.post(
 jobsRoute.delete(
 	"/delete",
 	zValidator("json", deleteFormSchema, (result) => {
-		if (!result.success) throw new HTTPException(400, { message: result.error.message });
+		if (!result.success) throw new HTTPException(400, { message: z.prettifyError(result.error) });
 	}),
 	jobsController.deleteJobData,
+);
+
+jobsRoute.patch(
+	"/update",
+	zValidator(
+		"json",
+		JobFormSchema.extend({ id: z.string().min(1, { error: "Job id is required to update job data" }) }),
+		(result) => {
+			if (!result.success) {
+				throw new HTTPException(400, { message: z.prettifyError(result.error) });
+			}
+		},
+	),
+
+	jobsController.updateJobData,
 );
 
 export default jobsRoute;
