7c366ad8b07290a64b72847086296bc498c45d67
Chillie <reskialamsyah@gmail.com>
2026-04-23 08:37:51 +0800

    update: add new route for delete job data

--- backend/src/routes/jobs.route.ts
+++ backend/src/routes/jobs.route.ts
@@ -1,11 +1,11 @@
-import { Hono } from "hono";
-import { checkAuth } from "@middlewares/check-auth.middleware";
-import { Variables } from "~/types/global.types";
-import { zValidator } from "@hono/zod-validator";
-import { JobFormSchema } from "~/types/jobs.types";
-import { HTTPException } from "hono/http-exception";
 import { jobsController } from "@controllers/jobs.controller";
+import { zValidator } from "@hono/zod-validator";
+import { checkAuth } from "@middlewares/check-auth.middleware";
 import { generalLimiter } from "@middlewares/rateLimiter.middleware";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { Variables } from "~/types/global.types";
+import { deleteFormSchema, JobFormSchema } from "~/types/jobs.types";
 
 // Base url is /api/jobs
 
@@ -13,8 +13,7 @@ const jobsRoute = new Hono<{ Variables: Variables }>();
 
 jobsRoute.use("/*", checkAuth, generalLimiter);
 
-// TODO: Add validation for query parameters
-jobsRoute.get("/", jobsController.getJobData);
+jobsRoute.get("/", jobsController.getJobsData);
 
 jobsRoute.post(
 	"/new",
@@ -24,6 +23,14 @@ jobsRoute.post(
 		}
 	}),
 	jobsController.create,
+);
+
+jobsRoute.delete(
+	"/delete",
+	zValidator("json", deleteFormSchema, (result) => {
+		if (!result.success) throw new HTTPException(400, { message: result.error.message });
+	}),
+	jobsController.deleteJobData,
 );
 
 export default jobsRoute;
