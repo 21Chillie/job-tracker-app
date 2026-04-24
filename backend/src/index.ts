6c7f33e5ce05b51b735c673de34d304553ad1c72
Chillie <reskialamsyah@gmail.com>
2026-04-24 15:46:13 +0800

    update: reorder imports and also add stats api route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
+import env from "@config/env";
+import logger from "@config/logger";
+import { dbShutdown } from "@db/db-client";
+import logMiddleware from "@middlewares/log.middleware";
+import authRoute from "@routes/auth.route";
+import jobsRoute from "@routes/jobs.route";
+import statsRoute from "@routes/stats.route";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
-import logMiddleware from "@middlewares/log.middleware";
-import env from "@config/env";
-import logger from "@config/logger";
-import authRoute from "@routes/auth.route";
-import jobsRoute from "@routes/jobs.route";
-import { dbShutdown } from "@db/db-client";
 import { runDatabaseMigration } from "./scripts/migrate-database";
 
 runDatabaseMigration();
@@ -14,48 +15,50 @@ runDatabaseMigration();
 const app = new Hono();
 
 app.use(
-  "*",
-  cors({
-    origin: (env.FRONTEND_URL as string) || "http://localhost:5173",
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization", "x-better-auth-origin"],
-    exposeHeaders: ["Content-Length", "Set-Cookie"],
-    maxAge: 600,
-    credentials: true,
-  }),
+	"*",
+	cors({
+		origin: (env.FRONTEND_URL as string) || "http://localhost:5173",
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowHeaders: ["Content-Type", "Authorization", "x-better-auth-origin"],
+		exposeHeaders: ["Content-Length", "Set-Cookie"],
+		maxAge: 600,
+		credentials: true,
+	}),
 );
 
 app.use("*", logMiddleware);
 
 app.get("/", (c) => {
-  return c.json({ message: "Hello from Hono!" });
+	return c.json({ message: "Hello from Hono!" });
 });
 
 app.route("/api/auth", authRoute);
 
 app.route("/api/jobs", jobsRoute);
 
+app.route("/api/stats", statsRoute);
+
 // Handle global error response
 app.onError((err, c) => {
-  const status = err instanceof HTTPException ? err.status : 500;
+	const status = err instanceof HTTPException ? err.status : 500;
 
-  if (env.BUN_APP_ENV === "development") {
-    console.error(err.stack);
-  }
+	if (env.BUN_APP_ENV === "development") {
+		console.error(err.stack);
+	}
 
-  logger.error(`${err.name}: ${err.message}`);
+	logger.error(`${err.name}: ${err.message}`);
 
-  return c.json(
-    {
-      success: false,
-      error: {
-        type: err.name || "InternalError",
-        message: err.message || "An unexpected error occurred",
-        stack: env.BUN_APP_ENV === "development" ? err.stack : undefined,
-      },
-    },
-    status,
-  );
+	return c.json(
+		{
+			success: false,
+			error: {
+				type: err.name || "InternalError",
+				message: err.message || "An unexpected error occurred",
+				stack: env.BUN_APP_ENV === "development" ? err.stack : undefined,
+			},
+		},
+		status,
+	);
 });
 
 process.on("SIGINT", dbShutdown);
