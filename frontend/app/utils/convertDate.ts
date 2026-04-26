9f7171d1df94e96df1cce06ba74239fb1b13380a
Chillie <reskialamsyah@gmail.com>
2026-04-26 10:34:05 +0800

    refactor: change formatDate and formatMonthly to named exports

--- frontend/app/utils/convertDate.ts
+++ frontend/app/utils/convertDate.ts
@@ -1,4 +1,4 @@
-export default function formatDate(dateInput: string) {
+export function formatDate(dateInput: string) {
   const formattedDate = new Date(
     dateInput.replace(/-/g, "/"),
   ).toLocaleDateString("en-US", {
@@ -10,10 +10,10 @@ export default function formatDate(dateInput: string) {
   return formattedDate;
 }
 
-export const formatMonthly = (dateString: string) => {
+export function formatMonthly(dateString: string) {
   const date = new Date(dateString);
   return date.toLocaleDateString("en-US", {
     month: "short", // "Feb"
     year: "numeric", // "2026"
   });
-};
+}
