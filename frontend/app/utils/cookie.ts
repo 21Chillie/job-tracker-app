916592f3dbe96d11c9b125948990ba840f142785
Chillie <reskialamsyah@gmail.com>
2026-08-20 05:24:15 +0800

    delete: remove frontend folder

--- frontend/app/utils/cookie.ts
+++
@@ -1,7 +0,0 @@
-export default function getSessionCookie(rawCookie: string) {
-  const cookies = new URLSearchParams(rawCookie.replace(/;\s*/g, "&"));
-  const sessionToken = cookies.get("job-tracker.session_token");
-  const secureSessionToken = cookies.get("__Secure-job-tracker.session_token");
-
-  return secureSessionToken ?? sessionToken;
-}
