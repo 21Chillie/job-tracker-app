bc5ae16afb6205b5afc7f708c7b4e1af530be912
Chillie <reskialamsyah@gmail.com>
2026-08-22 10:57:06 +0800

    add: global types

---
+++ types/global.type.ts
@@ -0,0 +1,31 @@
+export type ButtonProps = {
+  children: React.ReactNode;
+  variant?:
+    "default" | "outline" | "ghost" | "secondary" | "destructive" | "link";
+  size?:
+    "default" | "sm" | "lg" | "icon" | "xs" | "icon-xs" | "icon-sm" | "icon-lg";
+  className?: string;
+};
+
+export type ButtonLinkProps = ButtonProps & {
+  href: string;
+};
+
+export type AnyInputFieldProps = {
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  fieldDescription?: string;
+  defaultValues?: string;
+  icon?: React.JSX.Element;
+};
+
+export type TextInputFieldProps = AnyInputFieldProps & {
+  type: "text" | "email" | "search" | "url";
+};
+
+export type NumberInputFieldProps = AnyInputFieldProps & {
+  type: "number";
+  subType?: "default" | "price";
+};
