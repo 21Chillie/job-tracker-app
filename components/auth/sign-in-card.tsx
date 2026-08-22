14f2026bd4e6ab2a51470a0e453a88995fca1795
Chillie <reskialamsyah@gmail.com>
2026-08-22 08:37:48 +0800

    add: sign-up card with social login and separator

---
+++ components/auth/sign-in-card.tsx
@@ -0,0 +1,69 @@
+import SignUpForm from "@/components/auth/sign-up-form";
+import SocialSignInButton from "@/components/auth/social-sign-button";
+import SeparatorText from "@/components/global/separator-text";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import GoogleIcon from "@/public/assets/icons/google-color.svg";
+import GithubIcon from "@icons-pack/react-simple-icons/icons/SiGithub";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function SignInCard() {
+  return (
+    <Card className="w-full max-w-md">
+      <CardHeader className="border-b">
+        <CardTitle>Welcome back</CardTitle>
+        <CardDescription>
+          Ready to check and manage your job application? Please enter your
+          details below.
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <div className="flex flex-wrap gap-4 sm:gap-2">
+          <SocialSignInButton
+            variant="outline"
+            className="flex-1">
+            <Image
+              src={GoogleIcon}
+              width={16}
+              height={16}
+              alt="google icon"
+            />{" "}
+            Continue with Google
+          </SocialSignInButton>
+
+          <SocialSignInButton
+            variant="outline"
+            className="flex-1">
+            <GithubIcon />
+            Continue with Github
+          </SocialSignInButton>
+        </div>
+
+        <SeparatorText className="my-6">Or continue with email</SeparatorText>
+
+        <SignUpForm />
+      </CardContent>
+
+      <CardAction className="w-full border-t pt-6 text-center">
+        <p className="text-muted-foreground text-sm max-sm:mb-6">
+          Dont have an account?{" "}
+          <Link
+            className="text-foreground hover:underline"
+            href="/sign-up">
+            Sign up
+          </Link>
+        </p>
+      </CardAction>
+    </Card>
+  );
+}
