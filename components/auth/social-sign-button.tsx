21c8ded6b6f5bf3c5dabb1d2541af045491c7a33
Chillie <reskialamsyah@gmail.com>
2026-08-22 11:23:07 +0800

    chore: add TODO on social sign button

--- components/auth/social-sign-button.tsx
+++ components/auth/social-sign-button.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { ButtonProps } from "@/types/global.type";
 
+// TODO: finish social sign in button functionality GITHUB and GOOGLE (onclick)
 export default function SocialSigInnButton({
   children,
   className,
