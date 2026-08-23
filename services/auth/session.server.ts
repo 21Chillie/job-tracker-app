ca3226104ba44366313c7c1959504215ca1e28cc
Chillie <reskialamsyah@gmail.com>
2026-08-24 00:52:18 +0800

    feat(auth): check session cookie server action

---
+++ services/auth/session.server.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function checkSession() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/sign-in");
+  }
+
+  return session;
+}
