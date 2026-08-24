638dced578caa012723a2beabd747f9b0b21e73c
Chillie <reskialamsyah@gmail.com>
2026-08-25 03:02:45 +0800

    feat(auth): add option for email verification via otp

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,12 @@
 import prisma from "@/lib/prisma";
+import { sendEmailOTP } from "@/services/email/email-otp-action.server";
 import { updateUserRole } from "@/services/user/user-role.server";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
+import { emailOTP } from "better-auth/plugins";
+
+const enableVerification = process.env.RESEND_API_KEY ? true : false;
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,6 +14,13 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 128,
+    requireEmailVerification: enableVerification,
+  },
+  emailVerification: {
+    autoSignInAfterVerification: true,
+    sendOnSignUp: enableVerification,
   },
   socialProviders: {
     google: {
@@ -42,10 +53,25 @@ export const auth = betterAuth({
   },
 
   session: {
+    expiresIn: 60 * 60 * 24 * 7,
     cookieCache: {
       enabled: true,
       maxAge: 60 * 5,
     },
   },
-  plugins: [nextCookies()],
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        if (!enableVerification) return;
+        void sendEmailOTP({ to: email, otp, type });
+      },
+      otpLength: 6,
+      expiresIn: 600,
+      allowedAttempts: 3,
+      sendVerificationOnSignUp: true,
+      resendStrategy: "rotate",
+    }),
+
+    nextCookies(),
+  ],
 });
