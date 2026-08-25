264ca1946b8cce2bca80aec0de95b0bf6a45e86d
Chillie <reskialamsyah@gmail.com>
2026-08-25 14:03:39 +0800

    feat(email): finish functionality for sending email for otp code
    This features is optional and will be disable if doesn't provide
    required .env variables

--- services/email/email-otp-action.server.ts
+++ services/email/email-otp-action.server.ts
@@ -1,10 +1,13 @@
 "use server";
 
 import OTPEmailTemplate from "@/emails/otp-email-template";
-import { resend } from "@/lib/email";
+import getResend from "@/lib/email";
 import { OTPEmailType } from "@/types/auth.type";
 import { subjectMap } from "@/utils/auth-helper";
+import { randomUUIDv7 } from "bun";
 
+// This function sends an OTP email to the specified address using the Resend API.
+// If the RESEND_API_KEY key is not set, the function does nothing.
 export async function sendEmailOTP({
   to,
   otp,
@@ -17,11 +20,20 @@ export async function sendEmailOTP({
   try {
     const emailSubject = subjectMap[type];
 
+    const resend = getResend();
+    if (!resend) return;
+
+    console.log("Trying to sending otp code...");
     const { data, error } = await resend.emails.send({
-      from: process.env.EMAIL_FROM || "Acme <onboarding@resend.dev>",
+      from: process.env.EMAIL_FROM || "Job Tracker App <verify@resend.dev>",
       to,
       subject: emailSubject,
       react: OTPEmailTemplate({ otp, type }),
+      text: `Your verification code is: ${otp}\n\nThis code expires in 10 minutes.\nIf you didn't request this code, you can safely ignore this email.\n\nJob Tracker App`,
+
+      headers: {
+        "X-Entity-Ref-ID": randomUUIDv7(),
+      },
     });
 
     if (error) {
