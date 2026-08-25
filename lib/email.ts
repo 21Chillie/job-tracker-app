6c35bce276f97b476511471ea0bd212de028892f
Chillie <reskialamsyah@gmail.com>
2026-08-25 14:06:39 +0800

    feat(email): make resend email client optional
    Resend library is optional and will be disable if doesnt provide correct
    .env variables

--- lib/email.ts
+++ lib/email.ts
@@ -1,3 +1,12 @@
 import { Resend } from "resend";
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
+// This function returns a Resend instance if the RESEND_API_KEY key is set, otherwise returns null.
+// The use case of this is for self-hosted environments where the API key may not be available.
+export default function getResend() {
+  const apiKey = process.env.RESEND_API_KEY;
+
+  if (!apiKey) return null;
+
+  const resend = new Resend(apiKey);
+  return resend;
+}
