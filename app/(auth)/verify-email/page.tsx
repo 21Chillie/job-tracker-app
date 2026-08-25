0c4d29a1434eba5f28d8731595a15248bd3bfbaa
Chillie <reskialamsyah@gmail.com>
2026-08-25 14:12:58 +0800

    feat(auth): protect verify-email page and make sure the correct email can access it

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -1,5 +1,23 @@
 import OTPCard from "@/components/auth/otp-card";
+import { getUserEmail } from "@/services/user/user-data.server";
+import { redirect } from "next/navigation";
 
-export default function VerifyEmailPage() {
-  return <OTPCard type="email-verification" />;
+export default async function VerifyEmailPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  // Check if there is query params ?email=...
+  // Redirect to home if no email is provided
+  const { email } = (await searchParams) as { email: string | undefined };
+  if (typeof email !== "string") redirect("/");
+
+  // Check if there is email but not verified in database
+  // Redirect to home if no email it's not exist
+  const userEmailExist = await getUserEmail({ email, emailVerified: false });
+  if (!userEmailExist.data) redirect("/");
+
+  return (
+    <OTPCard type="email-verification" email={userEmailExist.data.email} />
+  );
 }
