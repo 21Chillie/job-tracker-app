f00505e52e078364bb309bdc2a88f4512c0ea955
Chillie <reskialamsyah@gmail.com>
2026-08-26 15:11:32 +0800

    style: change background color and adjust some layout

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -6,14 +6,16 @@ export default function LandingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col p-4 xl:px-0">
-      <header>
-        <Navbar />
-      </header>
+    <div className="bg-background flex min-h-screen w-full flex-col p-6 xl:px-0">
+      <div className="mx-auto flex w-full max-w-7xl flex-1 flex-col">
+        <header>
+          <Navbar />
+        </header>
 
-      <main className="my-6 flex-1">{children}</main>
+        <main className="my-6 flex-1">{children}</main>
 
-      <footer>Landing Footer</footer>
+        <footer>Landing Footer</footer>
+      </div>
     </div>
   );
 }
--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
       />
 
       {/* Headline */}
-      <h1 className="text-foreground/90 mx-auto mt-12 max-w-4xl text-3xl tracking-tight text-pretty sm:text-4xl md:text-7xl">
+      <h1 className="text-foreground/90 mx-auto mt-12 max-w-4xl text-4xl tracking-tight text-pretty md:text-7xl">
         The simple way
         <strong className="from-foreground/90 via-foreground/70 block bg-linear-to-r to-[color-mix(in_oklch,var(--primary)_85%,var(--foreground))] bg-clip-text font-medium text-transparent">
           manage your applications
