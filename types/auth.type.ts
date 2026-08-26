f658893ee2c71cc3548b1cc251b18e7979e0c6bb
Chillie <reskialamsyah@gmail.com>
2026-08-26 11:47:47 +0800

    feat(auth): for checking disposable email

--- types/auth.type.ts
+++ types/auth.type.ts
@@ -1,15 +1,16 @@
+import { emailSchema } from "@/lib/mail-checker";
 import { type User } from "@/prisma/generated/prisma/client";
 import z from "zod";
 
 export const SignInFormSchema = z.object({
-  email: z.email({ error: "Invalid email address" }).trim(),
+  email: emailSchema,
   password: z
     .string()
-    .min(8, { error: "Password must be at least 8 characters long" })
+    .min(8, { error: "Password must be at least 8 characters long." })
     .regex(/[A-Z]/, {
-      error: "Password must contain at least one uppercase letter",
+      error: "Password must contain at least one uppercase letter.",
     })
-    .regex(/[0-9]/, { error: "Password must contain at least one number" }),
+    .regex(/[0-9]/, { error: "Password must contain at least one number." }),
 });
 
 export const SignUpFormSchema = SignInFormSchema.extend({
