4ac13a90f418c547ae26a2d4f2fbdbd769c3fd25
Chillie <reskialamsyah@gmail.com>
2026-08-27 16:00:59 +0800

    style: adjust page width, padding, background color and also add metadata

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,11 @@
 import { ArrowLeft } from "lucide-react";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://jobtracker.chillie.my.id"),
+  title: "Authentication | Job Tracker App",
+};
 
 export default function AuthLayout({
   children,
@@ -7,10 +13,10 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto flex min-h-screen w-full flex-col p-2.5">
-      <main className="bg-background relative grid flex-1 place-items-center rounded-2xl p-6 sm:border sm:shadow-sm">
-        <div className="grid w-full place-items-center gap-4 sm:p-6">
-          <div className="flex w-full max-w-md items-center justify-between">
+    <div className="max-sm:bg-background mx-auto flex w-full flex-1 flex-col p-2.5 max-sm:p-4">
+      <main className="bg-background relative grid flex-1 place-items-center rounded-2xl max-sm:bg-transparent sm:shadow-sm">
+        <div className="grid w-full max-w-md place-items-center gap-4">
+          <div className="flex w-full items-center justify-between">
             <Link
               href={"/"}
               className="text-muted-foreground hover:text-foreground flex items-center gap-1 transition-colors"
