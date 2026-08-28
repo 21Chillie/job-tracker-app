335ff011935da76164785eff15b1a1f8d1aa1948
Chillie <reskialamsyah@gmail.com>
2026-08-28 09:08:57 +0800

    chore: change the file name

--- components/navigation/sidebar-menu-link.ts
+++ components/navigation/sidebar-menu-link.ts
@@ -10,26 +10,34 @@ export const destinations = [
   {
     label: "Dashboard",
     icon: LayoutGrid,
+    href: "/dashboard",
   },
   {
     label: "Manage Jobs",
     icon: CircleUserRound,
-    children: ["Job list", "Add jobs"],
+    children: [
+      { label: "Job list", href: "/dashboard/jobs" },
+      { label: "Add jobs", href: "/dashboard/jobs/new" },
+    ],
   },
   {
     label: "Resume",
     icon: FileText,
+    href: "/dashboard/resume",
   },
   {
     label: "Tasks",
     icon: ListTodo,
+    href: "/dashboard/tasks",
   },
   {
     label: "Notes",
     icon: NotebookTabs,
+    href: "/dashboard/notes",
   },
 ] satisfies {
   label: string;
+  href?: string;
   icon: typeof CircleUserRound;
-  children?: string[];
+  children?: { label: string; href?: string }[];
 }[];
