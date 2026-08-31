375be754b3efe9f9f182ee1e8863ea7b11ffa568
Chillie <reskialamsyah@gmail.com>
2026-08-31 09:48:10 +0800

    feat(auth): redirect signed-in users on sign-in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,11 @@
 import SignInCard from "@/components/auth/sign-in-card";
+import { checkSession } from "@/services/auth/auth-session.server";
+import { redirect } from "next/navigation";
 
-export default function LoginCard() {
+export default async function LoginCard() {
+const session = await checkSession()
+if (session) redirect('/')
+
+  
   return <SignInCard />;
 }
