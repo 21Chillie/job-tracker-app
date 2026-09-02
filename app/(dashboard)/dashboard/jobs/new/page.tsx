27324f0b68be0df6007d224f286475b28d9820fd
Chillie <reskialamsyah@gmail.com>
2026-09-02 21:36:34 +0800

    feat(jobs): add page header to new job page

--- app/(dashboard)/dashboard/jobs/new/page.tsx
+++ app/(dashboard)/dashboard/jobs/new/page.tsx
@@ -1,5 +1,19 @@
 import AddJobContainer from "@/components/job/add-job-container";
 
 export default function JobsNewPage() {
-  return <AddJobContainer />;
+  return (
+    <div>
+      <header className="space-y-2 mb-3 md:mb-6">
+        <h1 className="text-3xl font-medium tracking-tight sm:text-4xl">
+          Curate Opportunity
+        </h1>
+        <p className="text-muted-foreground text-sm leading-normal text-pretty sm:text-base md:leading-relaxed">
+          Keep your momentum going. Enter the role detais to start tracking you
+          progress.
+        </p>
+      </header>
+
+      <AddJobContainer />
+    </div>
+  );
 }
